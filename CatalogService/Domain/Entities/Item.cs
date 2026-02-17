07163bb55746be1982cb1c9c51b5551eb045a9e2
Kirus1930 <k_eryomin00@mail.ru>
2026-02-17 09:50:23 +0300

    Create Item.cs

---
+++ CatalogService/Domain/Entities/Item.cs
@@ -0,0 +1,12 @@
+using CatalogService.Domain.Enums;
+
+namespace CatalogService.Domain.Entities;
+
+public class Item
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public string Name { get; init; } = string.Empty;
+    public UnitOfMeasure Unit { get; init; }
+    public decimal PricePerUnit { get; init; }
+    public decimal QuantityInStock { get; init; }
+}
