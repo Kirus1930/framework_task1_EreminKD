d0e09035ec8f0020a1edb927524e0174f699baf5
Kirus1930 <k_eryomin00@mail.ru>
2026-02-17 09:57:19 +0300

    Create ItemValidator.cs

---
+++ CatalogService/Domain/Validation/ItemValidator.cs
@@ -0,0 +1,19 @@
+using CatalogService.Application.DTOs;
+using CatalogService.Domain.Exceptions;
+
+namespace CatalogService.Domain.Validation;
+
+public static class ItemValidator
+{
+    public static void Validate(CreateItemRequest request)
+    {
+        if (string.IsNullOrWhiteSpace(request.Name))
+            throw new DomainException("INVALID_NAME", "Название не может быть пустым");
+
+        if (request.PricePerUnit <= 0)
+            throw new DomainException("INVALID_PRICE", "Цена не может быть отрицательной или нулевой");
+
+        if (request.QuantityInStock < 0)
+            throw new DomainException("INVALID_QUANTITY", "Количество не может быть отрицательным");
+    }
+}
