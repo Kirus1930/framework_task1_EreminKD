d4fa696e591ba45e384fdabaa35bce4b847dbf5f
Kirus1930 <k_eryomin00@mail.ru>
2026-02-17 10:03:46 +0300

    Create InMemoryItemRepository.cs

---
+++ CatalogService/Infrastructure/Repositories/InMemoryItemRepository.cs
@@ -0,0 +1,27 @@
+using System.Collections.Concurrent;
+using CatalogService.Domain.Entities;
+
+namespace CatalogService.Infrastructure.Repositories;
+
+public class InMemoryItemRepository : IItemRepository
+{
+    private readonly ConcurrentDictionary<Guid, Item> _items = new();
+
+    public IEnumerable<Item> GetAll()
+    {
+        return _items.Values;
+    }
+
+    public Item? GetById(Guid id)
+    {
+        return _items.TryGetValue(id, out var item) ? item : null;
+    }
+
+    public Item Add(Item item)
+    {
+        if (!_items.TryAdd(item.Id, item))
+            throw new Exception("Не удалось добавить элемент");
+
+        return item;
+    }
+}
