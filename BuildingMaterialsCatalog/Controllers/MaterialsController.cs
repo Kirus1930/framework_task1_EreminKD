bf39d794772a1107f5ac9bc755438425d1bfbb51
Kirus1930 <k_eryomin00@mail.ru>
2026-02-23 14:50:53 +0300

    Update MaterialsController.cs

---
+++ BuildingMaterialsCatalog/Controllers/MaterialsController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using BuildingMaterialsCatalog.Models;
+using BuildingMaterialsCatalog.Services;
+using BuildingMaterialsCatalog.Validators;
+
+namespace BuildingMaterialsCatalog.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MaterialsController : ControllerBase
+{
+    private readonly IStorageService _storage;
+    private readonly BuildingMaterialValidator _validator;
+    private readonly ILogger<MaterialsController> _logger;
+
+    public MaterialsController(IStorageService storage, BuildingMaterialValidator validator, ILogger<MaterialsController> logger)
+    {
+        _storage = storage;
+        _validator = validator;
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var materials = _storage.GetAll();
+        return Ok(materials);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(Guid id)
+    {
+        var material = _storage.GetById(id);
+        if (material == null)
+            throw new KeyNotFoundException($"Material with id '{id}' not found.");
+        return Ok(material);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] BuildingMaterialCreateDto dto)
+    {
+        _validator.ValidateCreateDto(dto);
+        var material = await _storage.CreateAsync(dto);
+        _logger.LogInformation("Created material {MaterialId}", material.Id);
+        return CreatedAtAction(nameof(GetById), new { id = material.Id }, material);
+    }
+}
