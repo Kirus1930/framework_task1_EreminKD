8ed4d88e1e5c575a82c3fc37970ac7309b924f6b
Kirus1930 <k_eryomin00@mail.ru>
2026-02-24 09:18:59 +0300

    test dotnet run

--- BuildingMaterialsCatalog/obj/Debug/net10.0/BuildingMaterialsCatalog.AssemblyInfo.cs
+++ BuildingMaterialsCatalog/obj/Debug/net10.0/BuildingMaterialsCatalog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuildingMaterialsCatalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b88e21e96e73922155a6afbf4e88e758aa5bdb8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33d6c53e1074fd53932a9b8347c07045945490b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuildingMaterialsCatalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuildingMaterialsCatalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
